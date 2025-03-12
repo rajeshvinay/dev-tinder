4d0e77f1239eb6aeb2b26f61b22ee30a3aefd92b
rajeshvinay2012 <rajeshvinay2012@gmail.com>
2025-03-12 20:15:07 +0530

    Mongodb connection established.

--- src/app.js
+++ src/app.js
@@ -1,37 +1,13 @@
+const {connectDB} = require('./config/database')
 const express = require("express");
 
 const app = express();
 
-const {adminAuth,userAuth} = require('./middlewares/auth')
-
-app.use('/admin',adminAuth)
-
-app.get('/admin',(req,res)=>{
-    res.send(" generic admin ")
+connectDB().then(()=>{
+    app.listen(3000,(err,res)=>{
+        if(err) console.log(" error occured ",err)
+        else console.log("Database connection is established and the server is listening on port 3000")
+    });
+}).catch(()=>{
+    console.log("Unable to start the server because the database connection is not established.")
 })
-
-app.get('/admin/getAllData',(req,res)=>{
-    res.send(" Admin all get data ")
-})
-
-app.get('/admin/deleteUser',(req,res)=>{
-    res.send(" Deleted a User ")
-})
-
-app.get('/user/getUser',userAuth,(req,res)=>{
-    const userName = req.query.userdetails.userName
-    res.send(" User data fetched ",userName)
-})
-
-//catch all unhandled errors
-app.use('/',(err,req,res,next)=>{
-    if(err){
-        console.log(err)
-        res.status(500).send("An unexpected error happened. Please contact the support team.")
-    }
-})
-
-app.listen(3000,(err,res)=>{
-    if(err) console.log(" error occured ",err)
-    else console.log("server is listening on port 3000")
-});
