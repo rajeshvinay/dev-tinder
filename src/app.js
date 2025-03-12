2f57b5a51a9325a6421cf8ae875b5c679fd0b45e
rajeshvinay2012 <rajeshvinay2012@gmail.com>
2025-03-12 15:56:50 +0530

    route handlers with regex and middleares

--- src/app.js
+++ src/app.js
@@ -2,12 +2,25 @@ const express = require("express");
 
 const app = express();
 
-app.use((req,res)=>{
-    res.send(" hello fron test route")
+app.use("/user",(req,res,next)=>{
+    // res.send(" hello from middleware")
+    next();
 })
 
-app.use('*',(req,res)=>{
-    res.send(" all gibberish requests")
+// Regex based route with expects the end point to have A
+app.get(/A/,(req,res)=>{
+    res.send("actual user resp")
+})
+
+// dynamic route with params
+app.get("/user/:userId/:userName/:password",(req,res)=>{
+    const userDetails = req.params
+    console.log(userDetails)
+    res.send(" dynamic route ")
+})
+
+app.post("/user",(req,res)=>{
+    res.send("actual post user resp")
 })
 
 app.listen(3000,(err,res)=>{
